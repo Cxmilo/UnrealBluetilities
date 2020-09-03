fac4491063c01f13005ec503441f82b2d5421ace
Devmilo <camilo3ds@gmail.com>
2020-09-03 11:54:43 -0500

    [Programming] Starting Project

---
+++ udemyBluetilities/Source/udemyBluetilities/udemyBluetilities.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "udemyBluetilities.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, udemyBluetilities, "udemyBluetilities" );
---
+++ udemyBluetilities/Source/udemyBluetilities/udemyBluetilitiesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "udemyBluetilitiesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMYBLUETILITIES_API AudemyBluetilitiesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
